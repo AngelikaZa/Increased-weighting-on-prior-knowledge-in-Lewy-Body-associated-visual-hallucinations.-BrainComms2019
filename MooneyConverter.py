486a4d705c15e84927708d593e43dccbc2208284
Angelika Zarkali <42201483+AngelikaZa@users.noreply.github.com>
2019-06-28 09:36:11 +0100

    Add files via upload

---
+++ MooneyConverter.py
@@ -0,0 +1,47 @@
+""""
+Converts images to two-tone Mooney images
+Author: A. Zarkali
+Date: 10th September 2018
+"""
+
+import cv2
+import numpy as np
+import os, os.path
+
+template_path = r"C:\Users\Angelika\Dropbox\PhD\EXPERIMENTS\01_PriorsAmbiguousStimul\STIMULI\FINAL\Templates"
+size = (500, 500)
+
+templateImgs = []
+files = os.listdir(template_path)
+for f in range(len(files)):
+    templateImgs.append(os.path.join(template_path, files[f]))
+
+#Threshold image
+def apply_threshold(img, argument):
+    switcher = {
+        1: cv2.threshold(cv2.GaussianBlur(img, (9, 9), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
+        2: cv2.threshold(cv2.GaussianBlur(img, (7, 7), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
+        3: cv2.threshold(cv2.GaussianBlur(img, (5, 5), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
+    }
+    return switcher.get(argument, "Invalid method")
+
+def mooneyImage(path, method):
+    img = cv2.imread(path)
+    file_name = os.path.basename(path).split('.')[0]
+    file_name = file_name.split()[0]
+    print(path)
+
+    img = cv2.resize(img, size, interpolation=cv2.INTER_CUBIC)
+    img = cv2. cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+
+    img = apply_threshold(img, method)
+
+    save_path = os.path.join(file_name + "_filter_" + str(method) + ".jpg")
+    cv2.imwrite(save_path, img)
+
+for i in range(len(templateImgs)):
+    mooneyImage(templateImgs[i],1)
